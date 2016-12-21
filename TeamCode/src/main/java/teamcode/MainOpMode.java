847f7705b9f2a21db61c507c88afb0da061de800
nico icez <nico.v.murolo@gmail.com>
2016-12-21 03:34:55 -0600

    BIG CHANGES VERY GOOD

--- TeamCode/src/main/java/teamcode/MainOpMode.java
+++ TeamCode/src/main/java/teamcode/MainOpMode.java
@@ -9,16 +9,28 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Pushbot: Main Teleop", group="Pushbot")
 public class MainOpMode extends LinearOpMode {
-    PushbotMain robot;
+    PushbotMain robot = new PushbotMain();
     DcMotor rightFrontMotor = PushbotMain.rightFrontMotor;
     DcMotor leftFrontMotor = PushbotMain.leftFrontMotor;
     DcMotor rightBackMotor = PushbotMain.rightBackMotor;
     DcMotor leftBackMotor = PushbotMain.leftBackMotor;
-    double right_stick_y = PushbotMain.right_stick_y;
-    double left_stick_y = PushbotMain.left_stick_y;
+    public static boolean prev_a = false;
+    public static boolean prev_b = false;
+    public static boolean prev_dpad_down = false;
+    public static boolean prev_dpad_up = false;
+    public static boolean prev_dpad_left = false;
+    public static boolean prev_dpad_right = false;
+    public static double shooterPower = 0.20;
+    public static double enginePower = 0.8;
+    public static final double shooterIncr = 0.01;
+    public static final double engineIncr = 0.02;
+
+
+
 
     DualMotorGroup rightMotors = new DualMotorGroup(rightFrontMotor, rightBackMotor);
     DualMotorGroup leftMotors = new DualMotorGroup(leftFrontMotor, leftBackMotor);
+
 
     @Override
     public void runOpMode() {
@@ -30,11 +42,60 @@ public class MainOpMode extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            rightMotors.setPower((double) right_stick_y);
-            leftMotors.setPower((double) left_stick_y);
+            rightMotors.setPower((double) gamepad1.right_stick_y);
+            leftMotors.setPower((double) gamepad1.left_stick_y);
+
+            if (prev_dpad_up != gamepad1.dpad_up && gamepad1.dpad_up) {
+                shooterPower = incr(shooterPower, shooterIncr, "+");
+            }
+            if (prev_dpad_down != gamepad1.dpad_down && gamepad1.dpad_down) {
+                shooterPower = incr(shooterPower, shooterIncr, "-");
+            }
+            if (prev_dpad_left != gamepad1.dpad_left && gamepad1.dpad_left) {
+                enginePower = incr(enginePower, engineIncr, "-");
+            }
+            if (prev_dpad_right != gamepad1.dpad_right && gamepad1.dpad_right) {
+                enginePower = incr(enginePower, engineIncr, "+");
+            }
+
+            setValues(); //sets prev values to distinguish button presses tick-to-tick, preventing one press being
+                         //detected as 100 presses
         }
     }
     public void setValues() {
+        prev_a = gamepad1.a;
+        prev_b = gamepad1.b;
+        prev_dpad_down = gamepad1.dpad_down;
+        prev_dpad_left = gamepad1.dpad_left;
+        prev_dpad_right = gamepad1.dpad_right;
+        prev_dpad_up = gamepad1.dpad_up;
+    }
+    //a function that returns a modified value, checking if it falls within logical boundaries first
+    public double incr(double value, double incr, String sign) {
+        if (sign.equals("+")) {
+            if (value + incr <= 1.0) {
+                return value + incr;
+            }
+        } else if (sign.equals('-')) {
+            if (value - incr >= 0.0) {
+                return value - incr;
+            }
+        }
+        return value;
+    }
+
+    // startup shooter motors - they should remain spinning while the robot is running
+    public void startShooters() {
+
+    }
+
+    // pivot by `degrees` degrees
+    private void pivot(float degrees) {
+
+    }
+
+    // move forward/backword by `meters` meters
+    private void move(double meters) {
 
     }
 }
--- TeamCode/src/main/java/teamcode/PushbotMain.java
+++ TeamCode/src/main/java/teamcode/PushbotMain.java
@@ -19,30 +19,7 @@ public class PushbotMain {
     public static DcMotor screwMotor;
     public static DcMotor rightShooterMotor;
     public static DcMotor leftShooterMotor;
-    public boolean a;
-    public boolean b;
-    public boolean dpad_down;
-    public boolean dpad_up;
-    public boolean dpad_right;
-    public boolean dpad_left;
-    public float left_stick_x;
-    public static float left_stick_y = (float) 0.0;
-    public float right_stick_x;
-    public static float right_stick_y = (float) 0.0;
-    /*public boolean prev_a;
-    public boolean prev_b;
-    public boolean prev_dpad_down;
-    public boolean prev_dpad_up;
-    public boolean prev_dpad_left;
-    public boolean prev_dpad_right;  May be removed in future versions
-    public float prev_left_stick_x;
-    public float prev_left_stick_y;
-    public float prev_right_stick_x;
-    public float prev_right_stick_y;*/
-    public double shooterPower = 0.20;
-    public double enginePower = 1.0;
-    public double shooterIncr = 0.01;
-    public double engineIncr = 0.02;
+
 
     //runs on press of the "init" button. Maps engines from the robot to variables,
     // and creates a tick before the first with default values
@@ -55,44 +32,7 @@ public class PushbotMain {
         screwMotor = HM.dcMotor.get("Arc");
         //rightShooterMotor = HM.dcMotor.get("SR");
         //leftShooterMotor = HM.dcMotor.get("SL");
-        /*prev_a = false;
-        prev_b = false;
-        prev_dpad_down = false;
-        prev_dpad_up = false;
-        prev_dpad_right = false;
-        prev_dpad_left = false;
-        prev_left_stick_x = (float) 0.0;
-        prev_left_stick_y = (float) 0.0;
-        prev_right_stick_x = (float) 0.0;
-        prev_right_stick_y = (float) 0.0;*/
     }
 
-    //a function that returns a modified value, checking if it falls within logical boundaries first
-    public double incr(double value, double incr, String sign) {
-        if (sign.equals("+")) {
-            if (value + incr <= 1.0) {
-                return value + incr;
-            }
-        } else if (sign.equals('-')) {
-            if (value - incr >= 0.0) {
-                return value - incr;
-            }
-        }
-        return value;
-    }
 
-    // startup shooter motors - they should remain spinning while the robot is running
-    public void startShooters() {
-
-    }
-
-    // pivot by `degrees` degrees
-    private void pivot(float degrees) {
-
-    }
-
-    // move forward/backword by `meters` meters
-    private void move(double meters) {
-
-    }
 }
